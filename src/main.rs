01c509820cf7b02d899218969d952662b4f32313
gkourgkoutas <develop@onlogn.net>
2020-09-18 16:42:06 +0200

    implement structs + get wiki random title

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ use std::io::*;
 use std::path::Path;
 use structopt::StructOpt;
 
+fn main() {
+    let args = Cli::from_args();
+    
+    let content = Wikipedia::wiki_search(args.search);
+    Wikipedia::wiki_random();
+    Wordlist::create_output_file(&args.output);
+    Wordlist::create_wordlist(&content, args.output);
+}
+
 /// CLI Tool to generate wordlists based on wikipedia articles
 #[derive(StructOpt, Debug)]
 #[structopt(name = "w2w")]
@@ -15,75 +24,80 @@ struct Cli {
     /// Set the article language
     #[structopt(short = "l", long, default_value = "en")]
     lang: String,
+    // Random
+    /// Get random article
+    #[structopt(short = "r", long)]
+    random: Option<bool>,
     // Output
     /// Outputfile
     #[structopt(short, long)]
     output: String,
 }
 
-// Search Wikipedia
-// String -> String
-// Gets a keyword and returns the wikipedia page
-fn wiki_search(keyword: String) -> String {
-    let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
-    let page = wiki.page_from_title(keyword);
-    let content = page.get_content().unwrap();
-    return content;
-}
+struct Wikipedia {}
+impl Wikipedia {
+    // Search Wikipedia
+    // String -> String
+    // Gets a keyword and returns the wikipedia page
+    fn wiki_search(search: String) -> String {
+        let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
+        let page = wiki.page_from_title(search);
+        let content = page.get_content().unwrap();
+        return content;
+    }
 
-// Create Wikipedia wordlist
-// String, String -> File
-// Gets the wikipedia content, creates a wordlist
-fn create_wordlist(content: String, outfile: String){
-    // Create new wordlist file in current directory
-    let wordlist = content.split_whitespace();
-
-      for word in wordlist {
-        if word.len() >= 5 && 
-        (!word.contains('\'') || !word.contains(',') || !word.contains('\"') || !word.contains('.')){
-            let word = word.to_string() + "\n"; 
-            edit_output_file(word.to_string(), outfile.to_string());
-        }
+    // Get random wiki page
+    fn wiki_random() {
+        let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
+        let random_title = wiki.random().unwrap().take().unwrap().to_string();
+        println!("{}", random_title);
     }
 }
 
-// Helper function: Create new wordlist file
-fn create_output_file(path: String){
-    let path = Path::new(&path);
-    let display = path.display();
+struct Wordlist {}
+impl Wordlist {
+    // Create Wikipedia wordlist
+    // String, String -> File
+    // Gets the wikipedia content, creates a wordlist
+    fn create_wordlist(content: &str, outfile: String) {
+        // Create new wordlist file in current directory
+        let wordlist = content.split_whitespace();
 
-    match File::create(&path){
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-}
+        for word in wordlist {
+            if word.len() >= 5
+                && (!word.contains('\'')
+                    || !word.contains(',')
+                    || !word.contains('\"')
+                    || !word.contains('.'))
+            {
+                let word = word.to_string() + "\n";
+                Wordlist::edit_output_file(&word, &outfile);
+            }
+        }
+    }
 
-// Helper function: Edit wordlist file
-fn edit_output_file(word: String, outfile: String) {
-    let display_path = Path::new(&outfile).display();
-    
-    let mut file = OpenOptions::new()
-         .append(true)
-         .open(outfile.to_string())
-         .expect("No such file or directory");
-    
-    match file.write_all(word.as_bytes()){
-        Err(why) => panic!("couldn't write to {}: {}", display_path, why),
-        Ok(file) => file,
-    };
-}
+    // Helper function: Create new wordlist file
+    fn create_output_file(path: &str) {
+        let path = Path::new(&path);
+        let display = path.display();
 
-// Get random wiki page
-fn wiki_random() -> String {
-    let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
-    let random_page_title = wiki.random().unwrap();
-    let content = wiki_search(random_page_title.unwrap());
-    return content;
-}
+        match File::create(&path) {
+            Err(why) => panic!("couldn't open {}: {}", display, why),
+            Ok(file) => file,
+        };
+    }
 
-fn main() {
-    let args = Cli::from_args();
-    let wiki_content = wiki_search(args.search);
-    create_output_file(args.output.clone());
-    create_wordlist(wiki_content, args.output.clone());
+    // Helper function: Edit wordlist file
+    fn edit_output_file(word: &str, outfile: &str) {
+        let display_path = Path::new(&outfile).display();
+        let mut file = OpenOptions::new()
+            .append(true)
+            .open(outfile)
+            .expect("No such file or directory");
+
+        match file.write_all(word.as_bytes()) {
+            Err(why) => panic!("couldn't write to {}: {}", display_path, why),
+            Ok(file) => file,
+        };
+    }
 }
